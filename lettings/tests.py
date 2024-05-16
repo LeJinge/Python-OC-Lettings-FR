76093a2b29fe7259443840fe76cf995fc51b4a20
LeJinge <jeremycarmona28@gmail.com>
2024-05-16 14:55:00 +0200

    Réglage CircleCI

--- lettings/tests.py
+++ lettings/tests.py
@@ -161,6 +161,10 @@ class LettingsFunctionalTest(unittest.TestCase):
     def setUpClass(cls):
         chrome_options = Options()
         chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--disable-gpu")
+        chrome_options.add_argument("--window-size=1920,1080")
         cls.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
 
     @classmethod
